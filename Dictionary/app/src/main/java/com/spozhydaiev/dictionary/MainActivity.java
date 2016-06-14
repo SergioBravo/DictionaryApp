133e75db0c116fa51fc9620656299445589e4646
Serhii Pozhydaiev <revers.xz@gmail.com>
2016-06-15 00:12:55 +0300

    Created toolbar and styles

--- Dictionary/app/src/main/java/com/spozhydaiev/dictionary/MainActivity.java
+++ Dictionary/app/src/main/java/com/spozhydaiev/dictionary/MainActivity.java
@@ -2,11 +2,22 @@ package com.spozhydaiev.dictionary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
