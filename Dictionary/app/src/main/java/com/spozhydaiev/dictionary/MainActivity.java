45271df89315dca2535e401fb9935cd782f8f1e8
Serhii Pozhydaiev <revers.xz@gmail.com>
2016-06-16 00:29:27 +0300

    Created AutoCompleteTextView and Button

--- Dictionary/app/src/main/java/com/spozhydaiev/dictionary/MainActivity.java
+++ Dictionary/app/src/main/java/com/spozhydaiev/dictionary/MainActivity.java
@@ -3,10 +3,14 @@ package com.spozhydaiev.dictionary;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     private Toolbar toolbar;
+    private AutoCompleteTextView actv;
+    private Button translateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +18,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         initToolbar();
+        initAutoCompleteTextView();
+        initTranslateButton();
     }
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
+    }
+
+    private void initAutoCompleteTextView() {
+        actv = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+    }
+
+    private void initTranslateButton() {
+        translateBtn = (Button) findViewById(R.id.translateButton);
     }
 }
